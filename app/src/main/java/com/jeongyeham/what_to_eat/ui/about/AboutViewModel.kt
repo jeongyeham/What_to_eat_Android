6910a00a33ca81da2b4cb7f4af7e8d3e83268207
Ding Yihan <jeongyeham@foxmail.com>
2025-07-30 22:35:27 +0800

    refactor(app): simplify AboutViewModel by removing unused data
    - Remove MutableLiveData and LiveData usage in AboutViewModel
    - Delete unnecessary code to improve code clarity and performance

--- app/src/main/java/com/jeongyeham/what_to_eat/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/jeongyeham/what_to_eat/ui/about/AboutViewModel.kt
@@ -1,13 +1,7 @@
 package com.jeongyeham.what_to_eat.ui.about
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AboutViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = ""
-    }
-    val text: LiveData<String> = _text
+    // 简化ViewModel，不需要额外的数据
 }
