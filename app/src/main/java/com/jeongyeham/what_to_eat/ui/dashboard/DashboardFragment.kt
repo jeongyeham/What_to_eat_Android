6480717c8c7ed1796a0036162a0911425b1c813c
Ding Yihan <jeongyeham@foxmail.com>
2025-07-30 22:59:39 +0800

    refactor(ui): remove unused imports in DashboardFragment
    - Removed unused imports for Button, EditText, RecyclerView, and FoodAdapter
    - Simplified import statements to improve code readability

--- app/src/main/java/com/jeongyeham/what_to_eat/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/jeongyeham/what_to_eat/ui/dashboard/DashboardFragment.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.jeongyeham.what_to_eat.databinding.FragmentDashboardBinding
-import com.jeongyeham.what_to_eat.ui.dashboard.FoodAdapter
 
 class DashboardFragment : Fragment() {
 
